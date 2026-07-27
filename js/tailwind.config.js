a666d0b466c7a5c0973d18b30d94c941cb58a549
Asekhame Joel <asekhamejoel@gmail.com>
2026-07-27 03:40:10 +0100

    chore: install and sync dependencies in node_modules

---
+++ js/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ["./*.html", "./js/**/*.js"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter', 'system-ui', 'sans-serif'],
+                mono: ['"JetBrains Mono"', 'ui-monospace', 'monospace']
+            }
+        }
+    },
+    plugins: []
+}
